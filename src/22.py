3c70588cea428a2bb1920bb02f9971f765efad17
xavierbako60 <xavierbako60@gmail.com>
2025-04-01 14:35:33 +0000

    Add 22.py

---
+++ src/22.py
@@ -0,0 +1,9 @@
+def fibonacci_sequence(n):
+    sequence = []
+    a, b = 0, 1
+    while len(sequence) < n:
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+print(fibonacci_sequence(10))
